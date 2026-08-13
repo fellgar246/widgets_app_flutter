fd691b1239e660c4182bc121791cc72ff1b2c6c6
Luis Felipe Garcia <fellgar246@gmail.com>
2026-08-12 18:01:46 -0600

    Add title to MaterialApp in main.dart;

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -18,12 +19,42 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('¿Quieres salir de la aplicación?'),
+        content: const Text('Esta acción no se puede deshacer'),
+        actions: [
+          TextButton(
+              onPressed: () => context.pop(), child: const Text('Cancelar')),
+          FilledButton(
+              onPressed: () => context.pop(), child: const Text('Aceptar')),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbars y dialogos'),
       ),
+      body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        FilledButton.tonal(
+            onPressed: () {
+              showAboutDialog(
+                  context: context,
+                  children: [const Text('Información de la aplicación')]);
+            },
+            child: const Text('Licencias usadas')),
+        FilledButton.tonal(
+            onPressed: () => openDialog(context),
+            child: const Text('Mostrar dialogo')),
+      ])),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Mostrar Snackbar'),
         icon: const Icon(Icons.remove_red_eye_outlined),
